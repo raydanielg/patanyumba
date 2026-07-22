e5fb03b461ae6220114383bd934a3e142b494bc7
raydanielg <airezra2+1@gmail.com>
2026-07-22 09:10:04 +0300

    commit

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -6,7 +6,6 @@ import 'screens/splash_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/register_screen.dart';
 import 'screens/forgot_password_screen.dart';
-import 'screens/reset_password_screen.dart';
 
 void main() {
   runApp(const PatanyumbaApp());
