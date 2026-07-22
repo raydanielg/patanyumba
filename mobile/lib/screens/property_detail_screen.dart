85a47fe504107e8370f7f23d278ebbc79b0248b8
raydanielg <airezra2+1@gmail.com>
2026-07-22 14:17:17 +0300

    commit

--- mobile/lib/screens/property_detail_screen.dart
+++ mobile/lib/screens/property_detail_screen.dart
@@ -94,7 +94,9 @@ class _PropertyDetailScreenState extends State<PropertyDetailScreen> {
   }
 
   Widget _buildDetail() {
-    final images = (_property!['images'] as List<dynamic>?) ?? [];
+    final allMedia = (_property!['images'] as List<dynamic>?) ?? [];
+    final images = allMedia.where((m) => (m['media_type'] ?? 'image') == 'image').toList();
+    final videos = allMedia.where((m) => (m['media_type'] ?? 'image') == 'video').toList();
     final title = _property!['title'] ?? 'Untitled';
     final description = _property!['description'] ?? '';
     final price = _property!['price'];
