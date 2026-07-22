58f74f9014687bff8646e7a883f7b5edc0363d4c
raydanielg <airezra2+1@gmail.com>
2026-07-22 14:29:32 +0300

    commit

--- app/Models/PropertyCall.php
+++ app/Models/PropertyCall.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class PropertyCall extends Model
 {
-    //
+    protected $fillable = [
+        'property_id', 'caller_id', 'receiver_id',
+        'call_type', 'status', 'duration_seconds', 'contact_phone',
+    ];
+
+    public function property()
+    {
+        return $this->belongsTo(Property::class);
+    }
+
+    public function caller()
+    {
+        return $this->belongsTo(User::class, 'caller_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
 }
