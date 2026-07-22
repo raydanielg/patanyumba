a2287f6532da8ee7d60a952cd659519827713f3a
raydanielg <airezra2+1@gmail.com>
2026-07-22 13:37:02 +0300

    commit

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
-    //
+    protected $fillable = ['name', 'slug', 'icon', 'image', 'sort_order', 'is_active'];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($category) {
+            if (empty($category->slug)) {
+                $category->slug = Str::slug($category->name);
+            }
+        });
+    }
 }
