e8e8cd447de8d4e92b47868de535cb33f48199d9
raydanielg <airezra2+1@gmail.com>
2026-07-22 08:57:15 +0300

    commit

--- mobile/lib/constants/constants.dart
+++ mobile/lib/constants/constants.dart
@@ -4,12 +4,16 @@ class AppConstants {
   static const String appVersion = '1.0.0';
 
   // API
-  static const String baseUrl = 'http://localhost:8000/api';
+  // Use 10.0.2.2 for Android emulator (maps to host localhost)
+  // Use localhost for iOS simulator or web
+  // Use your actual server IP for physical devices
+  static const String baseUrl = 'http://10.0.2.2:8000/api';
   static const Duration apiTimeout = Duration(seconds: 30);
 
   // Storage Keys
   static const String tokenKey = 'auth_token';
   static const String userKey = 'user_data';
+  static const String onboardingKey = 'onboarding_complete';
 
   // Animation
   static const Duration splashDuration = Duration(seconds: 3);
