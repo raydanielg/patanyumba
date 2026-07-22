45ca2aff2f018c86b6e275e25e69bcc7db2a0998
raydanielg <airezra2+1@gmail.com>
2026-07-22 07:26:33 +0300

    commit

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -16,9 +16,40 @@ class SettingController extends Controller
 
     public function update(Request $request)
     {
-        foreach ($request->except('_token', '_method') as $key => $value) {
+        $data = $request->except('_token', '_method');
+
+        foreach ($data as $key => $value) {
             Setting::updateOrCreate(['key' => $key], ['value' => $value]);
         }
+
+        if ($request->expectsJson()) {
+            return response()->json(['success' => true, 'message' => 'Settings updated successfully']);
+        }
         return back()->with('status', 'Settings updated');
+    }
+
+    public function toggle(Request $request)
+    {
+        $request->validate([
+            'key' => 'required|string',
+        ]);
+
+        $setting = Setting::where('key', $request->key)->first();
+        if (!$setting) {
+            $setting = Setting::create(['key' => $request->key, 'value' => 'false', 'group' => 'features']);
+        }
+
+        $currentValue = $setting->value === 'true';
+        $setting->update(['value' => $currentValue ? 'false' : 'true']);
+
+        if ($request->expectsJson()) {
+            return response()->json([
+                'success' => true,
+                'message' => ucfirst(str_replace('_', ' ', $setting->key)) . ' ' . ($currentValue ? 'disabled' : 'enabled'),
+                'value' => $setting->value,
+                'enabled' => !$currentValue,
+            ]);
+        }
+        return back()->with('status', 'Setting toggled');
     }
 }
