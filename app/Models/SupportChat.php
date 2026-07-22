9f73b435bae717059f627d00c6c9ac8b1602ccac
raydanielg <airezra2+1@gmail.com>
2026-07-22 15:15:47 +0300

    commit

--- app/Models/SupportChat.php
+++ app/Models/SupportChat.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class SupportChat extends Model
 {
-    //
+    protected $fillable = ['user_id', 'subject', 'status', 'last_message_at'];
+
+    protected $casts = ['last_message_at' => 'datetime'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(SupportMessage::class);
+    }
+
+    public function unreadCount()
+    {
+        return $this->messages()->where('sender_type', 'user')->where('is_read', false)->count();
+    }
 }
