51942c092cb434d0a06cb40fb9ac4cb6619d021d
raydanielg <airezra2+1@gmail.com>
2026-07-22 15:35:50 +0300

    commit

--- app/Models/AboutContent.php
+++ app/Models/AboutContent.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class AboutContent extends Model
 {
-    //
+    protected $fillable = ['section', 'title', 'content', 'icon', 'image_url', 'stats', 'sort_order', 'is_active'];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+        'stats' => 'array',
+    ];
 }
