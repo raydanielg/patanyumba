968995904187b63644da93e64840c2e96f5cda58
raydanielg <airezra2+1@gmail.com>
2026-07-22 16:11:10 +0300

    commit

--- app/Models/District.php
+++ app/Models/District.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class District extends Model
 {
-    //
+    protected $fillable = ['region_id', 'name', 'sort_order', 'is_active'];
+
+    protected $casts = ['is_active' => 'boolean'];
+
+    public function region()
+    {
+        return $this->belongsTo(Region::class);
+    }
 }
