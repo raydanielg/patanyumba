f4cced084ad72b9e86e7fbcdcb182048e77226f0
raydanielg <airezra2+1@gmail.com>
2026-07-22 13:39:37 +0300

    commit

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,16 +2,27 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $categories = [
+            ['name' => 'Houses', 'icon' => 'home_work', 'sort_order' => 1],
+            ['name' => 'Apartments', 'icon' => 'apartment', 'sort_order' => 2],
+            ['name' => 'Rooms', 'icon' => 'bed', 'sort_order' => 3],
+            ['name' => 'Commercial', 'icon' => 'store', 'sort_order' => 4],
+            ['name' => 'Office', 'icon' => 'business', 'sort_order' => 5],
+            ['name' => 'Land', 'icon' => 'landscape', 'sort_order' => 6],
+        ];
+
+        foreach ($categories as $cat) {
+            Category::updateOrCreate(
+                ['name' => $cat['name']],
+                array_merge($cat, ['is_active' => true])
+            );
+        }
     }
 }
