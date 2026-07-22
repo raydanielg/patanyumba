a919a1633bcdbd4e9260577ee90d74951a235204
raydanielg <airezra2+1@gmail.com>
2026-07-22 15:16:12 +0300

    commit

--- app/Models/SupportMessage.php
+++ app/Models/SupportMessage.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class SupportMessage extends Model
 {
-    //
+    protected $fillable = ['support_chat_id', 'sender_type', 'message', 'is_read'];
+
+    protected $casts = ['is_read' => 'boolean'];
+
+    public function chat()
+    {
+        return $this->belongsTo(SupportChat::class, 'support_chat_id');
+    }
 }
